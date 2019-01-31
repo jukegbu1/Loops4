ee5eb61ff223b1df5e29409de94d1bdd0d35e4ca
jukegbu1 <jukegbu1@gmail.com>
2019-01-31 14:13:33 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Even Numbers");
+        for(int i=1; i<=20; i++) {
+            if (i%2!=1){
+                System.out.println(i + "");
+            }
+        }
+    } }
